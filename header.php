b2deaa39e1c587ea55d2f0fa2f49327437ff56fd
Maxim Ustianov <maksym.ustianov@p-product.com>
2021-08-12 17:21:57 +0300

    nav menu

--- header.php
+++ header.php
@@ -94,12 +94,11 @@ $header_menus = wp_get_nav_menu_items( $header_menu_id );
 					<!-- /Top Nav -->
 
 					<!-- Main Nav -->
-					<div id="nav-bottom">
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<div class="container">
+                        <ul class="nav-menu">
 							<?php
 								if ( ! empty( $header_menus ) && is_array( $header_menus ) ) {
 									?>
-                                    <ul class="navbar-nav mr-auto">
 										<?php
 											foreach ( $header_menus as $menu_item ) {
 												if ( ! $menu_item->menu_item_parent ) {
@@ -110,28 +109,35 @@ $header_menus = wp_get_nav_menu_items( $header_menu_id );
 													
 													if ( ! $has_children ) {
 														?>
-                                                        <li class="nav-item">
-                                                            <a class="nav-link" href="<?php echo esc_url( $menu_item->url ); ?>">
+                                                        <li>
+                                                            <a href="<?php echo esc_url( $menu_item->url ); ?>">
 																<?php echo esc_html( $menu_item->title ); ?>
                                                             </a>
                                                         </li>
 														<?php
 													} else {
 														?>
-                                                        <li class="nav-item dropdown">
-                                                            <a class="nav-link dropdown-toggle" href="<?php echo esc_url( $menu_item->url ); ?>" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                        <li class="has-dropdown">
+                                                            <a href="<?php echo esc_url( $menu_item->url ); ?>">
 																<?php echo esc_html( $menu_item->title ); ?>
                                                             </a>
-                                                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-																<?php
-																	foreach ( $child_menu_items as $child_menu_item ) {
-																		?>
-                                                                        <a class="dropdown-item" href="<?php echo esc_url( $child_menu_item->url ); ?>">
-																			<?php echo esc_html( $child_menu_item->title ); ?>
-                                                                        </a>
-																		<?php
-																	}
-																?>
+                                                            <div class="dropdown">
+                                                                <div class="dropdown-body">
+                                                                    <ul class="dropdown-list">
+				                                                        <?php
+					                                                        foreach ($child_menu_items as $child_menu_item) {
+						                                                        ?>
+                                                                                <li>
+                                                                                    <a class="dropdown-item"
+                                                                                       href="<?php echo esc_url($child_menu_item->url); ?>">
+								                                                        <?php echo $child_menu_item->title ?>
+                                                                                    </a>
+                                                                                </li>
+						                                                        <?php
+					                                                        }
+				                                                        ?>
+                                                                    </ul>
+                                                                </div>
                                                             </div>
                                                         </li>
 														<?php
@@ -141,12 +147,10 @@ $header_menus = wp_get_nav_menu_items( $header_menu_id );
 												}
 											}
 										?>
-                                    </ul>
 									<?php
 								}
 							?>
-							<?php get_search_form(); ?>
-                        </div>
+                        </ul>
 					</div>
 					<!-- /Main Nav -->
 
